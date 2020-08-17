374529cf5e40365e3c7f3a61f30602ca5e0ff3e5
DeeDeeG <DeeDeeG@users.noreply.github.com>
2020-08-16 22:22:39 -0400

    console.log() whether SECRET_VAR matched the expected value

--- thing.js
+++ thing.js
@@ -1,3 +1,9 @@
+if (process.env.SECRET_VAR === '1234') {
+  console.log('SECRET_VAR matched expectation!')
+} else {
+  console.log('SECRET_VAR did not match expectation!')
+}
+
 console.log('SECRET_VAR is:' + process.env.SECRET_VAR)
 console.log('NONSECRET_VAR is:' + process.env.NONSECRET_VAR)
 console.log('UNSET_VAR is:' + process.env.UNSET_VAR)
